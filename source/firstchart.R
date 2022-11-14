21cfe9036be3fde669ff6e6df6de62a22b41bb23
Anahi <anavz13@uw.edu>
2022-11-14 14:56:17 -0800

    Updated

--- source/firstchart.R
+++ source/firstchart.R
@@ -1,1 +1,9 @@
 # First chart for project
+library("ggplot2")
+df <- read.csv(file = '~/info201/assignments/project-pmgialis/data/SaYoPillow.csv')
+
+data.frame(df)
+
+graph1 <- ggplot(data = df, mapping = aes(x = lm, y = sr.1)) +
+  geom_point()
+graph1
