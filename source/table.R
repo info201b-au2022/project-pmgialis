996d45c49b6a26b05fb5075a656f529730357e2d
Anahi <anavz13@uw.edu>
2022-11-14 20:39:44 -0800

    Work is Completed

--- source/table.R
+++ source/table.R
@@ -3,4 +3,13 @@ df <- read.csv(file = '../data/SaYoPillow.csv')
 View(df)
 library(stringr)
 library(dplyr)
-library(tidyverse)
+library("tidyverse")
+
+df_group <- df %>%
+  group_by(sr.1) %>%
+  summarise(temps = t,
+            .groups = 'drop')
+
+round(df_group$sr.1, 1)
+round(df_group$temps, 1)
+View(df_group)
